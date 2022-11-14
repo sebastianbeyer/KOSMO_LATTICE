7bc426e903a7e5073d63651b4c3284782929bceb
Sebastian Beyer <sebastian.beyer@riseup.net>
2022-11-14 20:23:06 +0100

    add upload script

---
+++ upload.sh
@@ -0,0 +1,2 @@
+/home/sebastian/bin/arduino-1.8.15/hardware/tools/avr/bin/avrdude -C/home/sebastian/bin/arduino-1.8.15/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyUSB0 -b576000 -D -Uflash:w:grids.hex:i
+
