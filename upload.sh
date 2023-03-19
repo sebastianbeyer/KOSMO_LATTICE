b07ff6cdc75850c4bf32a0c7a2e49afd1393a3b5
Sebastian Beyer <sebastian.beyer@riseup.net>
2023-03-19 21:01:51 +0100

    add another upload line

--- upload.sh
+++ upload.sh
@@ -1,2 +1,4 @@
 /home/sebastian/bin/arduino-1.8.15/hardware/tools/avr/bin/avrdude -C/home/sebastian/bin/arduino-1.8.15/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyUSB0 -b576000 -D -Uflash:w:grids.hex:i
 
+/home/sebastian/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude -C/home/sebastian/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyUSB0 -b57600 -D -Uflash:w:/tmp/arduino_build_309185/Blink.ino.hex:i
+
